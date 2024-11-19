d0e5d10bec43fd5968a9db28283e6e1d64f62023
Shxmpathy <christiankeough1@gmail.com>
2024-11-18 20:47:26 -0500

    made mole disappear after clicking its space by taking coords of mouse press and determining if = to mole pos, move mole

--- whackamole.py
+++ whackamole.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 
 
 def main():
@@ -11,11 +11,9 @@ def main():
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
         running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-            screen.fill("light green")
+        mole_coord = (0,0)
+
+        def create_grid():
             for i in range(1, 16):
                 ##rows grid drawing
                 pygame.draw.line(screen,
@@ -25,15 +23,39 @@ def main():
                                  1
 
                                  )
-            for i in range(1,20):
-                #col grid drawing
+            for i in range(1, 20):
+                # col grid drawing
                 pygame.draw.line(screen,
-                                 (0,0,0),
-                                 (i*32, 0),
-                                 (i*32, 640),
+                                 (0, 0, 0),
+                                 (i * 32, 0),
+                                 (i * 32, 640),
                                  1
                                  )
+
+        screen.fill("light green")
+        create_grid()
+        screen.blit(mole_image, mole_image.get_rect(topleft=((0), (0))))
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = event.pos
+                    print(x//32, y//32)
+                    mouse_pos = (x//32, y//32)
+                    if mouse_pos == mole_coord:
+                        screen.fill("light green")
+                        create_grid()
+                        pygame.display.flip()
+                        rand_num1 = random.randrange(0, 640)
+                        rand_num2 = random.randrange(0,480)
+                        # screen.blit(mole_image, mole_image.get_rect(space =((rand_num1), (rand_num2))))
+
+
+
+            mole_coord = (0,0)
             pygame.display.flip()
+
             clock.tick(60)
 
     finally:
